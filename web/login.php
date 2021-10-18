34311b9f21d7661b6c7fbf611157effddd2b6dd4
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-10-18 18:52:27 -0400

    Bare login form

---
+++ web/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="login.php" method="post">
+        <label for="input-username">
+            Username
+            <input type="text" name="username" id="input-username">
+        </label>
+        <label for="input-password">
+            Password
+            <input type="password" name="password" id="input-password">
+        </label>
+    </form>
+</body>
+
+</html>
