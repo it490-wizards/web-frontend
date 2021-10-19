cb479f78c02a37f7c489ec8e389e236058f586b7
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-10-18 23:35:28 -0400

    Added non-null requirements to user schema

--- schema/user.sql
+++ schema/user.sql
@@ -1,6 +1,7 @@
-CREATE TABLE user(
-    user_id         int PRIMARY KEY AUTO_INCREMENT,
-    username        varchar(64),
-    password_hash   binary(32), -- SHA-256 produces a 32-byte digest
-    salt            binary(16)
+CREATE TABLE `user` (
+    `user_id`       int NOT NULL AUTO_INCREMENT,
+    `username`      varchar(64) NOT NULL,
+    `password_hash` binary(32) NOT NULL, -- SHA-256 produces a 32-byte digest
+    `salt`          binary(16) NOT NULL,
+    PRIMARY KEY (`user_id`)
 );
