098a5b3ec1e4a1da917aad855ff3c9b680dd99cd
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-10-20 12:41:17 -0400

    Require unique usernames

--- schema/user.sql
+++ schema/user.sql
@@ -3,5 +3,6 @@ CREATE TABLE `user` (
     `username`      varchar(64) NOT NULL,
     `password_hash` binary(32) NOT NULL, -- SHA-256 produces a 32-byte digest
     `salt`          binary(16) NOT NULL,
-    PRIMARY KEY (`user_id`)
-);
+    PRIMARY KEY (`user_id`),
+    UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_bin;
