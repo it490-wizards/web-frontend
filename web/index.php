748aaa41ebeff638d1fa85e23a65854892453e4d
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-10-22 16:26:49 -0400

    User login (#1)
    * Bare login form
    
    * Empty homepage with login link
    
    * Simple user table schema
    
    * Added registration page
    
    * Registration with salted+hashed passwords
    
    * Added non-null requirements to user schema
    
    * Require unique usernames
    
    * Register and login prototype working

---
+++ web/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Home</h1>
+    <p>
+        <a href="login.php">Login</a>
+    </p>
+    <p>
+        <a href="register.php">Register</a>
+    </p>
+</body>
+
+</html>
